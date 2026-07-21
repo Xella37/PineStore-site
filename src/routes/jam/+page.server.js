ea4fdbef09ae30dca94ca1dbad1595a637a3f535
Xella <XeloStar39@gmail.com>
2026-07-21 12:34:46 +0200

    Improved jams page

--- src/routes/jam/+page.server.js
+++ src/routes/jam/+page.server.js
@@ -12,7 +12,31 @@ export async function load({ params, cookies }) {
 	if (!jamData.success)
 		throw error(404, jamData.error);
 
+	let jams = jamData.jams.map(jam => {
+		let now = Date.now();
+		let status = "unknown";
+		if (now < jam.date_start)
+			status = "starting";
+		else if (now < jam.date_end)
+			status = "ongoing";
+		else if (!jam.judging_finished)
+			status = "judging";
+		else
+			status = "results";
+		
+		return {
+			id: jam.id,
+			title: jam.title,
+			date_start: jam.date_start,
+			date_end: jam.date_end,
+			judging_finished: jam.judging_finished,
+			status: status,
+		}
+	}).sort((a, b) => {
+		return b.date_start - a.date_start;
+	});
+
 	return {
-		jams: jamData.jams,
+		jams: jams,
 	};
 };
--- src/routes/jam/+page.svelte
+++ src/routes/jam/+page.svelte
@@ -32,6 +32,13 @@
 			year: "numeric",
 		});
 	}
+
+	const STATUS_MAP = {
+		"starting": "Starting soon",
+		"ongoing": "Active now",
+		"judging": "Judging in progress",
+		"results": "Results out",
+	};
 </script>
 
 <div id="backgroundContainer"></div>
@@ -39,19 +46,37 @@
 <div class="page-container">
 	<div class="page page-thin shadow">
 		<h1>
-			Jams
+			PineStore Jams
 		</h1>
+
+		<p>
+			Here you can find all the jams that have been held on PineStore so far. You can click on a jam to view its submissions, results, and other information.
+		</p>
+
+		<div class="ruler-text">
+			<span>all jams</span>
+		</div>
 
 		<div class="jam-list">
 			{#each jams as jam}
 				<a href="/jam/{jam.id}" class="no-link">
-					<div class="jam">
+					<div class="jam" class:status-starting={jam.status === "starting"} class:status-ongoing={jam.status === "ongoing"} class:status-judging={jam.status === "judging"} class:status-results={jam.status === "results"}>
 						<h2>{jam.title}</h2>
-						<span>{formatShortDate(jam.date_start)} / {formatShortDate(jam.date_end)}</span>
+						<span class="period">{formatShortDate(jam.date_start)} / {formatShortDate(jam.date_end)}</span>
+
+						<span class="status">{STATUS_MAP[jam.status]}</span>
+
+						<img src="{BASE_URL}/{jam.id}.png" alt="Banner for {jam.title}" class="jam-logo" />
 					</div>
 				</a>
 			{/each}
 		</div>
+
+		{#if !jams.find(jam => jam.id === "pinejam2023")}
+			<p style="margin-top: 1rem;">
+				Note: PineJam 2023 is not listed here yet. You can view it <a href="https://jam.pine3d.cc/jam/2023" target="_blank">here</a>.
+			</p>
+		{/if}
 	</div>
 </div>
 
@@ -77,34 +102,75 @@
 	.jam-list {
 		display: flex;
 		flex-direction: column;
-		gap: 1.5rem;
+		gap: 1rem;
+		padding-left: 2rem;
 	}
 
 	.jam {
 		position: relative;
-		padding-left: 2rem;
-		transition: transform ease 125ms;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		/* min-height: 8rem; */
+		--status-color: #FFF;
+		border-radius: 1rem;
+		padding: 1rem;
 	}
 	.jam:hover {
-		transform: translateX(1rem);
+		background-color: var(--cc-gray);
+	}
+
+	.jam.status-starting {
+		--status-color: var(--cc-green);
+	}
+	.jam.status-ongoing {
+		--status-color: var(--cc-green);
+	}
+	.jam.status-judging {
+		--status-color: var(--cc-cyan);
+	}
+	.jam.status-results {
+		--status-color: var(--cc-blue);
 	}
 
 	.jam::before {
 		position: absolute;
 		content: "";
-		background-color: var(--cc-lime);
+		background-color: var(--status-color);
 		border-radius: 1rem;
 		top: 0;
 		bottom: 0;
-		left: 0;
+		left: -2rem;
 		width: 1rem;
 	}
 
 	.jam h2 {
+		margin-top: 0;
 		margin-bottom: 0.5rem;
 	}
 
-	.jam span {
+	.jam span.period {
 		color: var(--text-color-dark);
+	}
+
+	.jam span.status {
+		display: block;
+		margin-top: 1rem;
+		color: var(--text-color);
+		background-color: var(--status-color);
+		font-size: 1.25rem;
+		max-width: fit-content;
+		padding: 0.25rem 1rem;
+		border-radius: 2rem;
+	}
+
+	.jam img {
+		position: absolute;
+		top: 50%;
+		right: 1rem;
+		max-width: 20rem;
+		height: calc(min(8rem, 100% - 2rem));
+		transform: translateY(-50%);
+		object-fit: contain;
 	}
 </style>
