b18071b6e3b18c802d003af347174c77a78d5eda
Xella <XeloStar39@gmail.com>
2026-09-11 14:10:31 +0200

    Display char count for short description

--- src/routes/profile/edit/[id]/ConfigDescription.svelte
+++ src/routes/profile/edit/[id]/ConfigDescription.svelte
@@ -21,6 +21,8 @@
 <div class="tab-container form-list" class:selected={selectedDescription == "short"}>
 	<label for="descriptionShortInput">Description Short. Used for meta tags and short text for PineStore clients. Try to keep this under ~150 characters.</label>
 	<textarea id="descriptionShortInput" type="text" bind:value={project.description_short} maxlength="200" placeholder="Short description, under ~150 characters"></textarea>
+
+	<div class="char-count" class:over-limit={project.description_short?.length > 150}>{project.description_short?.length ?? 0} chars</div>
 </div>
 
 <div class="tab-container form-list" class:selected={selectedDescription == "main"}>
@@ -46,6 +48,7 @@
 
 <style>
 	.tab-container {
+		position: relative;
 		display: none;
 		margin-top: 1rem;
 	}
@@ -69,6 +72,23 @@
 		flex-grow: 1;
 		margin-top: 0;
 		font-size: 1rem;
+	}
+
+	.char-count {
+		position: absolute;
+		bottom: 1rem;
+		right: 1rem;
+		padding: 0.5rem 1rem;
+		background-color: var(--cc-lightBlue);
+		margin: 0;
+		border-radius: 1rem;
+		color: black;
+	}
+	.char-count.over-limit {
+		background-color: var(--cc-red);
+		color: white;
+		font-weight: bold;
+		font-size: 1.25rem;
 	}
 
 	@media (max-width: 600px) {
